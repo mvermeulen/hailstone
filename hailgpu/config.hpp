a67bd600c397f429fdd3fffe4ff17409f2aad1b3
Mike Vermeulen <mike@vermeulen.com>
2019-02-05 17:40:27 -0600

    Fix up issues with 2^24 single case.

--- hailgpu/config.hpp
+++ hailgpu/config.hpp
@@ -4,5 +4,6 @@
 #define POLY_WIDTH              10 // 3^10 == 59049 fits in 16 bits
 
 typedef unsigned short digit_t;    // 16-bit
+typedef unsigned int wdigit_t;     // 24-bit
 #define MAX_DIGIT                8 // 128-bit
 
--- hailgpu/hailmax.cpp
+++ hailgpu/hailmax.cpp
@@ -7,6 +7,7 @@ extern int maxcutoff_width;
 extern unsigned int maxcutoff_value[];
 
 digit_t global_max[MAX_DIGIT] = { 0 };
+wdigit_t global_wmax[MAX_DIGIT] = { 0 };
 int global_max_n = 1;
 
 void report_peak(digit_t *n){
@@ -20,6 +21,23 @@ void report_peak(digit_t *n){
       continue;
     } else {
       std::cout << " " << global_max[i];
+      highdigit = 1;
+    }
+  }
+  std::cout << " for <" << n[3] << "," << n[2] << "," << n[1] << "," << n[0] << ">" << std::dec << std::endl;
+}
+
+void report_wpeak(wdigit_t *n){
+  int i;
+  int highdigit = 0;
+  std::cout << std::hex << "Peak in values:";
+  for (i=MAX_DIGIT-1;i>=0;i--){
+    if (highdigit){
+      std::cout << " " << global_wmax[i];
+    } else if (global_wmax[i] == 0){
+      continue;
+    } else {
+      std::cout << " " << global_wmax[i];
       highdigit = 1;
     }
   }
@@ -186,6 +204,54 @@ void hailmax32p(digit_t *n,int *maxfound){
   } while (1);
 }
 
+// search for maximum value up to 2^24
+void hailwmax24p(wdigit_t *n,int *maxfound){
+  unsigned int n0 = n[0], n1 = 0;
+  unsigned int lastbits;
+  unsigned int mask = ((1u << POLY_WIDTH)-1);
+  unsigned int shiftcnt;
+  unsigned int mul3val;
+  unsigned int start = n0;
+  //  std::cout << "hailwmax24p - " << n0 << std::endl;      
+  do {
+    // std::cout << "val - " << n0 << std::endl;    
+    lastbits = n0 & mask;
+    n0 = n0 & ~mask;
+    // shift by power of 2
+    if (shiftcnt = poly_table[lastbits].pow2){
+      n0 = (n0 >> shiftcnt) | (n1 << (24-shiftcnt));
+      n1 = n1 >> shiftcnt;
+      n0 &= 0xffffff;
+    }
+    // multiply by power of 3 and add
+    mul3val = poly_table[lastbits].pow3;
+    n0 = n0 * mul3val + poly_table[lastbits].add;
+    n1 = n1 * mul3val + (n0 >> 24);
+    n0 &= 0xffffff;
+    // check for greater or less
+    if (poly_table[lastbits].gtone){
+      if (n1 > global_wmax[1]){
+	*maxfound = 1;
+	global_wmax[0] = n0;
+	global_wmax[1] = n1;
+	continue;
+      } else if (n1 < global_wmax[1]){
+	continue;
+      }
+      // n1 == global_max[1]
+      if (n0 > global_wmax[0]){
+	// std::cout << "max - " << n0 << std::endl;
+	*maxfound = 1;
+	global_wmax[0] = n0;
+	global_wmax[1] = n1;
+      }
+      continue;
+    } else {
+      if ((n1 == 0) && (n0 <= start)) return;
+    }
+  } while (1);
+}
+
 void search_block0(void){
   unsigned int i,j;
   int maxfound = 0;
@@ -212,15 +278,23 @@ void search_block0(void){
   }  
 }
 
+void wsearch_block0(void){
+  unsigned int i,j;
+  int maxfound = 0;
+  wdigit_t n[MAX_DIGIT] = { 0 };
+
+  for (j=1;j<(1l<<24);j++){
+    n[0] = j;
+    hailwmax24p(n,&maxfound);
+    if (maxfound){
+      report_wpeak(n);
+      maxfound = 0;
+    }
+  }
+}
+
 int main(void){
   int maxfound = 0;
-  /*
-  digit_t n[MAX_DIGIT] = { 0 };
-  n[0] = 5;
-  n[1] = 1;
-  hailmax32(n,&maxfound);
-  hailmax32p(n,&maxfound);
-  return 0;
-  */
+  wsearch_block0();
   search_block0();
 }
